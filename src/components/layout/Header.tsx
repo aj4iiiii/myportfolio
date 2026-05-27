d04b6bbf402493be36331d1c4d9c3c2267c39b27
Ajay V <ajay.vyleri2005@gmail.com>
2026-05-27 21:49:35 +0530

    Make header responsive for mobile view

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='w-full font-display bg-[url("/paper.png")] z-50 bg-repeat fixed text-4xl text-[#9F9F9F] items-center hidden  h-20 bg-transparent lg:flex justify-between px-20'>
+    <div className='w-full font-display bg-[url("/paper.png")] z-50 bg-repeat fixed text-2xl sm:text-3xl lg:text-4xl text-[#9F9F9F] items-center h-16 lg:h-20 bg-transparent flex justify-between px-6 sm:px-12 lg:px-20'>
       <div>AV</div>
-      <div className='flex gap-10'>
+      <div className='flex gap-4 sm:gap-10'>
         <Link href='/'>home</Link>
         <Link href='/about'>about</Link>
         <Link href='/contact'>contact</Link>
